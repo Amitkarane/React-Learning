a3516e16d5641b6bd83c3bf29fe2ccb0cde7aecf
Amitkarane <amitkarane449@gmail.com>
2025-11-16 14:14:53 +0530

    added dark mode and light mode feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [myStyle,setMyStyle] = useState({
+    color:'black',
+    background:'linear-gradient(to left, #fdd1b7ff, #b2b9f5ff)'
+  })
+  const [btnText,setBtnText] = useState('Enable Dark Mode');
+
+  let toggleMode = ()=>{
+    if(myStyle.color === 'black'){
+      setMyStyle({
+        color:'white',
+        background:'linear-gradient(to left, #000000, #434343)'
+      })
+      setBtnText('Enable Default Mode')
+    }
+    else{
+      setMyStyle({
+        color:'black',
+        background:'linear-gradient(to left, #fdd1b7ff, #b2b9f5ff)'
+      })
+      setBtnText('Enable Dark Mode')
+    }
+  }
   return (
-    <>
+    <div style={myStyle}>
     <div className="NavBar">
-    <Navbar title="PRIME GAMING" link="Discord" />
+    <Navbar title="PRIME GAMING" link="Discord" toggleMode={toggleMode} btnText={btnText} />
     </div>
     <br /><br />
     <div className="container">
@@ -15,7 +39,7 @@ function App() {
 
     
     <br /><br />
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,10 +19,11 @@ export default function Navbar(props) {
           <a className="nav-link" href="/">{props.link}</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
+      <button className="btn btn-secondary" onClick={props.toggleMode}>{props.btnText}</button>
+      {/* <form class="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      </form> */}
     </div>
   </div>
 </nav>
